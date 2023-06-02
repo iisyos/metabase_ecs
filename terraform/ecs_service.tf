8d05ab0663dc54ea72ada6f70ad7bfc5f285df0e
iisyos <kosuke1216a@yahoo.co.jp>
2023-06-03 00:27:47 +0900

    fix: make ECS security group accept Metabase port (3000)

--- terraform/ecs_service.tf
+++ terraform/ecs_service.tf
@@ -83,8 +83,8 @@ resource "aws_security_group_rule" "ecs" {
 
   type = "ingress"
 
-  from_port = 80
-  to_port   = 80
+  from_port = 3000
+  to_port   = 3000
   protocol  = "tcp"
 
   source_security_group_id = aws_security_group.alb.id
